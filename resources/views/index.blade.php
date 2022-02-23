813c0a3fd1b5ce6a292496311a771cf4ea765eaa
dedyfuadi92 <dedyfuadi92@gmail.com>
2022-02-24 05:42:28 +0800

    vercel test 4 - css bootstrap 1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="/../css/bootstrap.min.css">
     {{-- manifest --}}
     <link rel="manifest" href="manifest.json">
 
