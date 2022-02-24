14fa143cf6854f25ef31e92ea7c74395a259eed0
dedyfuadi92 <dedyfuadi92@gmail.com>
2022-02-24 10:44:01 +0800

    kembali ke awal

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="/css/bootstrap.min.css">
     {{-- manifest --}}
-    <link rel="manifest" href="/pwa/manifest.json">
+    <link rel="manifest" href="/manifest.json">
 
     <title>Laravel PWA</title>
 </head>
